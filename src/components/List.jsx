f5e393311f260b675660391f969157ca8da672d5
Gerdkid <joshgerding@gmail.com>
2015-12-16 10:25:05 -0500

    Set up simple list skeleton with basic structure

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,15 +1,15 @@
 var React = require('react');
 var ListItem = require('./ListItem.jsx');
 
-var ingredient = [{"id":1, "text": "ham"}, {"id":2, "text": "cheese"}, {"id":3, "text": "beef"}];
+var ingredients = [{"id":1, "text": "ham"}, {"id":2, "text": "cheese"}, {"id":3, "text": "beef"}];
 
 var List = React.createClass({
 	render: function () {
-		var listItems = ingredients.map(function (map) {
-			return <ListItem key={item.id} ingredient={item.text}/>;
+		var listItems = ingredients.map(function (item) {
+			return (<ListItem key={item.id} ingredient={item.text}/>);
 		});
 
-		return (<ul>(listItems)</ul>);
+		return (<ul>{listItems}</ul>);
 	}
 });
 
